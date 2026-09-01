1b974e2eada1ca3f8f2e522a581735519668e839
kkjjwwww <kgw2034@gmail.com>
2026-09-01 11:08:37 +0900

    script prefab folder 분리

---
+++ Assets/01.Scripts/UI/UI_Stats_Tab.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class UI_Stats_Tab : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> tabBGSources;
+    [SerializeField] private List<Button> tabButtons;
+    [SerializeField] private List<GameObject> contentPanels;
+}
