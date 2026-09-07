7937057f5912d010f90488be8bed875f87213e74
jeonsisung970226-code <jeonsisung970226@gmail.com>
2026-09-07 18:36:34 +0900

    웨이브매니져 1차수정

--- Assets/01.Scripts/JeonSiseong/WaveManager.cs
+++ Assets/01.Scripts/JeonSiseong/WaveManager.cs
@@ -20,27 +20,30 @@ public class WaveManager : MonoBehaviour
 
 
 
-
+    [Header("Boss")]
     [SerializeField] GameObject bossPrefab;    // 보스 몬스터 프리팹
     [SerializeField] Transform bossSpawnPoint;  // 보스 스폰 포인트
 
-
+    [Header("Monster Spawn")]
     [SerializeField] EnemySpawn[] spawnPoints;    //  몬스터 스폰 포인트 배열
-
     [SerializeField] float spawnInterval = 1.2f;  // 몬스터 스폰 속도
-
     [SerializeField] int maxAliveCount = 6;   // 최대 생존 몬스터 수 
 
+    [Header("Wave")]
     [SerializeField] int maxWave = 10;         
+    [SerializeField] int maxKillCount =10;
 
-    [SerializeField] int maxKillCount =10;    
 
+    [Header("Boss Timer")]
     [SerializeField] float bossTimeLimit = 30f;    // 보스 클리어 시간제한
     float bossTimer;
 
+    [Header("UI")]
     [SerializeField] TMP_Text waveCountText;        //     텍스트 추가
     [SerializeField] TMP_Text bossTimerText;        //     텍스트 추가
 
+    [Header("Next Wave/Stage Delay")]
+    [SerializeField] float nextWaveDelay = 3f;    //  다음 웨이브 딜레이 시간
 
 
     int aliveCount = 0;
@@ -59,6 +62,10 @@ public class WaveManager : MonoBehaviour
 
     Coroutine bossTimerRoutine;
     Coroutine spawnRoutine;
+
+    Coroutine nextWaveRoutine;
+    Coroutine bossDelayRoutine;
+    Coroutine nextStageRoutine;
 
 
     void StartSpawn()
@@ -117,11 +124,23 @@ public class WaveManager : MonoBehaviour
     public void EnemyKilled()
     {
         aliveCount--;
+
+        if(aliveCount < 0)
+        {
+            aliveCount = 0;
+        }
+
+
         killCount++;
 
         if(waitingForBoss && aliveCount <= 0)
         {
-            StartBossBattle();
+
+            if(bossDelayRoutine == null)
+            {
+                bossDelayRoutine = StartCoroutine(BossDelay());
+            }
+            
             return;
         }
 
@@ -131,9 +150,71 @@ public class WaveManager : MonoBehaviour
 
         if(killCount >=  maxKillCount)
         {
-            NextWave();
+
+
+            if(currentWave >= maxWave)
+            {
+                waitingForBoss = true;
+
+                if(spawnRoutine != null)
+                {
+                    StopCoroutine(spawnRoutine);
+                    spawnRoutine = null;
+                }
+
+                if(aliveCount <= 0 && bossDelayRoutine==null)
+                {
+                    bossDelayRoutine= StartCoroutine(BossDelay());
+                }
+            }
+            else
+            {
+                if(nextWaveRoutine == null)
+                {
+                    nextWaveRoutine = StartCoroutine(NextWaveDelay());
+                }
+            }
+
         }
     }
+
+    IEnumerator BossDelay()
+    {
+        if(spawnRoutine !=  null)
+        {
+            StopCoroutine(spawnRoutine);
+            spawnRoutine = null;
+        }
+
+
+        yield return new WaitForSeconds(nextWaveDelay);
+
+        bossDelayRoutine = null;
+
+        StartBossBattle();
+    }
+
+
+
+    IEnumerator NextWaveDelay()
+    {
+
+        if(spawnRoutine != null)
+        {
+            StopCoroutine(spawnRoutine);
+            spawnRoutine = null;
+        }
+
+        yield return new WaitForSeconds(nextWaveDelay);
+
+        nextWaveRoutine = null;
+
+        NextWave();
+    }
+
+
+
+
 
     void NextWave()
     {
@@ -141,9 +222,11 @@ public class WaveManager : MonoBehaviour
         {
             waitingForBoss = true;
 
-            if(aliveCount <= 0)
+            if(aliveCount <= 0 && bossDelayRoutine == null)
             {
-                StartBossBattle();
+
+                bossDelayRoutine = StartCoroutine(BossDelay());
+                
             }
             
 
@@ -151,6 +234,8 @@ public class WaveManager : MonoBehaviour
         }
         currentWave++;
         killCount = 0;
+
+        StartSpawn();
     }
 
     void StartBossBattle()
@@ -161,12 +246,20 @@ public class WaveManager : MonoBehaviour
             return;
         }
 
+        if(currentBoss != null)
+        {
+            return;
+        }
+
 
         Debug.Log("보스출현");
 
         isBossBattle = true;
+        waitingForBoss = true;
+        bossFinish = false;
 
         bossTimerText.gameObject.SetActive(true);
+
 
         currentBoss =
         Instantiate(bossPrefab, bossSpawnPoint.position, Quaternion.identity);
@@ -196,6 +289,7 @@ public class WaveManager : MonoBehaviour
 
     public void BossKilled()
     {
+
         if(bossFinish)    // 보스 사망 함수가 두번 호출 됐을 때, 스테이지 두번 올라가는 것을 방지
         {
             return;
@@ -203,20 +297,48 @@ public class WaveManager : MonoBehaviour
 
         bossFinish = true;
 
+        Debug.Log("보스처치 다음 스테이지 시작");
+
+
         bossTimerText.gameObject.SetActive(false);
 
+
+        // 보스 타이머 정지
         if(bossTimerRoutine != null)
         {
             StopCoroutine(bossTimerRoutine);
             bossTimerRoutine = null;
         }
 
+        currentBoss = null;
+
+        if(nextStageRoutine==null)
+        {
+            nextStageRoutine = StartCoroutine(NextStageDelay());
+        }
+
+        
+    }
+
+    IEnumerator NextStageDelay()
+    {
+        yield return new WaitForSeconds(nextWaveDelay);
+
+        nextStageRoutine = null;
+
         NextStage();
     }
 
 
+
     void NextStage()
     {
+       
+
+        Debug.Log("다음 스테이지 시작");
+
+
+
         isBossBattle = false;
 
         currentWave = 1;
@@ -243,6 +365,11 @@ public class WaveManager : MonoBehaviour
             currentBoss = null;
         }
 
+        if(bossTimerRoutine != null)
+        {
+            StopCoroutine(bossTimerRoutine);
+            bossTimerRoutine = null;
+        }
 
 
         waitingForBoss = false;
