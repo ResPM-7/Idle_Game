9cc038071e7df759d456c6702abde26923c2ede8
KIM <respm9011@gmail.com>
2026-09-18 10:43:09 +0900

    스킬 오브젝트 폴링으로 변경

--- Assets/01.Scripts/JeonSiseong/Skills/Skill/FireSkill.cs
+++ Assets/01.Scripts/JeonSiseong/Skills/Skill/FireSkill.cs
@@ -1,64 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class FireSkill : Skill
 {
+    [SerializeField] private string poolName = "Fire";
+    [SerializeField] private float duration = 1f;
 
-    //[Header("Fire Settings")]
-    //[SerializeField] float radius = 5f;         // 스킬 범위 반지름
-    //[SerializeField] float damage = 20;          // 스킬 데미지
+    private void OnEnable()
+    {
+        StartCoroutine(ReturnToPoolAfterDelay());
+    }
 
+    private IEnumerator ReturnToPoolAfterDelay()
+    {
+        yield return new WaitForSeconds(duration);
 
-    //[Header("Target")]
-    //[SerializeField] LayerMask enemyLayer;
+        if (ObjectPoolManager.instance != null)
+        {
+            ObjectPoolManager.instance.ReturnObject(poolName, gameObject);
+        }
+    }
 
-
-    //void DamageEnemy()
-    //{
-    //    Collider2D[] enemies =Physics2D.OverlapCircleAll(transform.position,radius,enemyLayer);
-
-    //    foreach(Collider2D enemy in enemies)
-    //    {
-    //        ISkillDamageable target = enemy.GetComponent<ISkillDamageable>();
-
-    //        if(target != null)
-    //        {
-    //            target.TakeSkillDamage(damage);
-    //        }
-    //    }
-    //}
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (((1 << collision.gameObject.layer) & enemyLayer.value) != 0)
-        //{ 
-
-        //    ISkillDamageable target = collision.GetComponent<ISkillDamageable>();
-        //    if (target != null)
-        //    {
-        //        Debug.Log("파이어 데미지");
-        //        target.TakeSkillDamage(damage);
-        //    }
-        //}
-
         DamageTarget(collision);
-
-
-
     }
-
-
-    void Start()
-    {
-        //DamageEnemy();
-
-        Destroy(gameObject,1f);
-    }
-
-
-    //private void OnDrawGizmosSelected()
-    //{
-
-    //    Gizmos.DrawWireSphere(transform.position, radius);
-    //}
-
-   
 }
--- Assets/01.Scripts/JeonSiseong/Skills/Skill/LightningSkill.cs
+++ Assets/01.Scripts/JeonSiseong/Skills/Skill/LightningSkill.cs
@@ -1,71 +1,28 @@
+using System.Collections;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class LightningSkill : Skill
 {
+    [SerializeField] private string poolName = "Lightning";
+    [SerializeField] private float duration = 1f;
 
-    //[Header("Lightning Settings")]
-    //[SerializeField] float radius = 1.5f;    // 스킬 범위 반지름
-    //[SerializeField] float damage = 100;       // 스킬 데미지
+    private void OnEnable()
+    {
+        StartCoroutine(ReturnToPoolAfterDelay());
+    }
 
+    private IEnumerator ReturnToPoolAfterDelay()
+    {
+        yield return new WaitForSeconds(duration);
 
-    //[Header("Target")]
-    //[SerializeField] LayerMask enemyLayer;
-
-
-    //void DamageEnemy()
-    //{
-    //    Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, radius,enemyLayer);
-
-    //    foreach (Collider2D enemy in enemies)
-    //    {
-    //        ISkillDamageable target = enemy.GetComponent<ISkillDamageable>();
-
-    //        if(target != null)
-    //        {
-    //            target.TakeSkillDamage(damage);
-    //        }
-    //    }
-    //}
+        if (ObjectPoolManager.instance != null)
+        {
+            ObjectPoolManager.instance.ReturnObject(poolName, gameObject);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (((1 << collision.gameObject.layer) & enemyLayer.value) != 0)
-        //{
-
-        //    ISkillDamageable target = collision.GetComponent<ISkillDamageable>();
-        //    if (target != null)
-        //    {
-        //        target.TakeSkillDamage(damage);
-        //    }
-        //}
-
         DamageTarget(collision);
-
-
     }
-
-
-
-    //private void OnDrawGizmosSelected()
-    //{
-
-    //    Gizmos.DrawWireSphere(transform.position, radius);
-    //}
-
-
-
-
-
-
-
-    void Start()
-    {
-        //DamageEnemy();
-
-        Destroy(gameObject,1f);
-    }
-
-    
-    
 }
